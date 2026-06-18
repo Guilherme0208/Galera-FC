bbde7c43e51d4385a4a503afd3ae174a39b4e24a
Guilherme Henrique Freitas Silva <ghfs.tc@gmail.com>
2026-06-18 19:07:37 -0300

    Criar admin temporario

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,27 @@
-"""
-URL configuration for config project.
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.conf import settings
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+
+def criar_admin_temporario(request):
+    token = request.GET.get('token')
+
+    if token != 'galera-fc-admin-2026':
+        return HttpResponse('Acesso negado.', status=403)
+
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='ghfs.tc@gmail.com',
+            password='Admin@123456'
+        )
+
+        return HttpResponse('Superusuário criado com sucesso.')
+
+    return HttpResponse('Superusuário já existe.')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,4 +40,10 @@ urlpatterns = [
     ),
     
     path('', include('financeiro.urls')),
+    
+    path(
+        'criar-admin-temporario/',
+        criar_admin_temporario,
+        name='criar_admin_temporario'
+    ),
 ]
